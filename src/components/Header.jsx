74ac40bc2f912e58b98538f10969cf7fbe791fbf
Enzo Soliz <enzosoliz95@gmail.com>
2023-04-06 13:05:52 -0300

    next/image: assets en Next.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import Image from "next/image.js";
 import Menu from '../components/Menu.jsx';
 import menu from '../assets/icons/icon_menu.svg';
 import logo from '../assets/logos/logo_yard_sale.svg'
@@ -12,9 +13,9 @@ const Header = () => {
     return (
             <>
                 <nav className={styles.nav}>
-                    <img src={menu} alt="menu" className={styles['menu']} />
+                    <Image src={menu} alt="menu" className={styles['menu']} />
                     <div className={styles['navbar-left']}>
-                        <img src={logo} alt="logo" className={styles['nav-logo']} />
+                        <Image src={logo} alt="logo" className={styles['nav-logo']} />
                         <ul>
                             <li>
                                 <a href="/">All</a>
@@ -45,7 +46,7 @@ const Header = () => {
                                 className={styles['navbar-shopping-cart']}
                                 onClick={() => toggleOrder()}
                             >
-                                <img className={`${styles['more-clickable-area']} ${styles.pointer}`} src={shoppingCart} alt="shopping cart" />
+                                <Image className={`${styles['more-clickable-area']} ${styles.pointer}`} src={shoppingCart} alt="shopping cart" />
                                 {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
                             </li>
                         </ul>
