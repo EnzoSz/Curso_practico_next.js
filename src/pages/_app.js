890e4d1fcb2eaaa1da9b624b35d23a1c73572ce3
Enzo Soliz <enzosoliz95@gmail.com>
2023-04-06 12:22:50 -0300

    React context en el header

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css';
-
+import AppContext from '../context/AppContext.js';
+import useInitialState from '../hooks/useInitialState.js';
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const initialState = useInitialState();
+  return (
+    <AppContext.Provider value={initialState}>
+      <Component {...pageProps} />
+    </AppContext.Provider>
+    
+  );
 }
 
 export default App;
